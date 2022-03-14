330a1efbe3d2e9189dba6252c4ebb73c367f4b90
Kyle-Xu001 <xuchenghao10@hotmail.com>
2022-03-14 17:26:21 +0100

    finish the distortion for single image

--- image_feature_extraction_test.py
+++ image_feature_extraction_test.py
@@ -72,7 +72,7 @@ class Image(object):
     
 if __name__ == '__main__':
     # Load exmaple image
-    img = cv.imread("lamp_15_distorted_empty.JPG")
+    img = cv.imread("lamp_18_distorted_empty.PNG")
     img = np.rot90(img,1) # Rotate the image to get better visualization
     
     Img = Image(img)
--- undistortion.py
+++ undistortion.py
@@ -6,6 +6,10 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
+import utils
+from image_feature_extraction_test import Image
+import image_match_test as match_utils
+
 # Load the intrinsic parameter for every lamp
 def load_params(calib_dir):
     balance = 0
@@ -28,7 +32,7 @@ def load_params(calib_dir):
 
     return calib_params_all,center_offsets,balance,right_extend
 
-
+# Define the undistortion function to generate map1 & map2
 def calculate_map(img,DIM,mtx,dist,balance,center_offset=0,right_extend=0,map1 = None, map2 = None):
     dim1 = img.shape[:2][::-1]  #dim1 is the dimension of input image to un-distort
     #assert dim1[0]/dim1[1] == DIM[0]/DIM[1]
@@ -40,29 +44,13 @@ def calculate_map(img,DIM,mtx,dist,balance,center_offset=0,right_extend=0,map1 =
     new_K = cv.fisheye.estimateNewCameraMatrixForUndistortRectify(scaled_K, dist, dim2, np.eye(3), balance=balance)
     new_K[0,2] += center_offset
     map1, map2 = cv.fisheye.initUndistortRectifyMap(scaled_K, dist, np.eye(3), new_K, dim3, cv.CV_32FC1)
-    
-    
+
     return map1,map2
 
-
-def undistort(img,DIM,mtx,dist,balance,center_offset=0,right_extend=0,map1 = None, map2 = None):
-
-    map1, map2 = calculate_map(img,DIM,mtx,dist,balance,center_offset,right_extend)
-    undistorted_img = cv.remap(img, map1, map2, interpolation=cv.INTER_LINEAR, borderMode=cv.BORDER_CONSTANT)
-    
-    return undistorted_img,map1,map2
-
-if __name__ =='__main__':
-    # Define the operating lamp
-    lamp_id = 'lamp14'
-    img = cv.imread("lamp_14_distorted_empty.JPG")
-    
-    # Enter the direction of the parameters
-    calib_dir = "/home/cxu-lely/kyle-xu001/Multi-Depth-Multi-Camera-Stitching/calib_params_Mathe"
-    
+# Generate the undistorted image and corresponding mapping function
+def undistort(img,lamp_id,calib_dir,map1 = None, map2 = None):
     # Load the calibration
     calib_params_all,center_offsets,balance,right_extend = load_params(calib_dir)
-    
     
     DIM, mtx, dist = calib_params_all[lamp_id]
     center_offset = center_offsets[lamp_id]
@@ -72,10 +60,126 @@ if __name__ =='__main__':
     scale = img_width/DIM[0]
     right_extend = int(right_extend*scale)
     center_offset = int(center_offset*scale)
-    img_size = (img_width+right_extend,
-                img_height)
-    img_undistort,map1,map2 = undistort(img,DIM,mtx,dist,balance,center_offset,right_extend)
+    
+    map1, map2 = calculate_map(img,DIM,mtx,dist,balance,center_offset,right_extend)
+    undistorted_img = cv.remap(img, map1, map2, interpolation=cv.INTER_LINEAR, borderMode=cv.BORDER_CONSTANT)
+    return undistorted_img,map1,map2
+
+if __name__ =='__main__':
+    # Define the operating lamp
+    lamp_id1 = 'lamp19'
+    lamp_id2 = 'lamp18'
+    
+    # Load the distorted images
+    img1 = cv.imread("lamp_19_distorted_empty.PNG")
+    img2 = cv.imread("lamp_18_distorted_empty.PNG")
+    
+    # Enter the direction of the parameters
+    calib_dir = "/home/cxu-lely/kyle-xu001/Multi-Depth-Multi-Camera-Stitching/calib_params_Mathe"
+    
+    # Calculate the mapping matrix
+    img_undistort1,map1_1,map2_1 = undistort(img1,lamp_id1,calib_dir)
+    img_undistort2,map1_2,map2_2 = undistort(img2,lamp_id2,calib_dir)
     
     plt.figure(1)
-    plt.imshow(cv.cvtColor(img_undistort, cv.COLOR_BGR2RGB))
+    plt.subplot(2,2,1)
+    plt.imshow(cv.cvtColor(img1, cv.COLOR_BGR2RGB))
+    plt.title('(a) Original Distorted Image [%s]'%(lamp_id1))
+    plt.axis('off')
+    plt.subplot(2,2,2)
+    plt.imshow(cv.cvtColor(img_undistort1, cv.COLOR_BGR2RGB))
+    plt.title('(b) Undistorted Image [%s]'%(lamp_id1))
+    plt.axis('off')
+    plt.subplot(2,2,3)
+    plt.imshow(cv.cvtColor(img2, cv.COLOR_BGR2RGB))
+    plt.title('(c) Original Distorted Image [%s]'%(lamp_id2))
+    plt.axis('off')
+    plt.subplot(2,2,4)
+    plt.imshow(cv.cvtColor(img_undistort2, cv.COLOR_BGR2RGB))
+    plt.title('(d) Undistorted Image [%s]'%(lamp_id2))
+    plt.axis('off')
+    
+    # Define the draw parameters for matching visualization
+    draw_params = dict(matchColor = (0,255,0),
+                    singlePointColor = (0,0,255),
+                    flags = cv.DrawMatchesFlags_DEFAULT)
+
+    # load the matching images
+    img1 = np.rot90(img1,1) 
+    img2 = np.rot90(img2,1)
+
+    if (img1.shape[0]>img1.shape[1]):
+    # Manually define the ROI to locate the area for corresponding images
+        # ROIs1 = [
+        #     [450, 950, 768, 1300],
+        #     [450, 750, 768, 1000],
+        #     [450, 450, 768, 750],
+        #     [450, 250, 768, 450]]
+        # ROIs2 = [
+        #     [0, 950,350, 1300],
+        #     [0, 750, 350, 1000],
+        #     [0, 500, 350, 800],
+        #     [0, 250, 350, 500]]
+        ROIs1 = [
+            #[450, 850, 768, 1300],
+            [450, 625, 768, 900],
+            #[450, 300, 768, 625],
+            [450, 150, 768, 350]]
+        ROIs2 = [
+            #[0, 900,350, 1300],
+            [0, 700, 350, 950],
+            #[0, 450, 350, 750],
+            [0, 150, 350, 450]]
+    else:
+        ROIs1 = [
+            [100, 400, 350, 767],
+            [300, 400, 500, 767],
+            [500, 400, 950, 767],
+            [800, 400, 1100, 767]]
+
+        ROIs2 = [
+            [100, 0, 450, 400],
+            [350, 0, 550, 400],
+            [550, 0, 1000,400],
+            [800, 0, 1150, 400]]
+        # ROIs1 = [
+        #     [50, 450, 300, 767],
+        #     [300, 450, 576, 767],
+        #     [576, 450, 876, 767],
+        #     [876, 450, 1126, 767]]
+
+        # ROIs2 = [
+        #     [0, 0, 250, 400],
+        #     [276, 0, 476, 400],
+        #     [476, 0, 776, 400],
+        #     [776, 0, 1126, 400]]
+
+    # Initialize the object
+    Img1 = Image(img1)
+    Img2 = Image(img2)
+
+    # Extract the features from each images
+    kps1, des1 = Img1.findFeatures('sift')
+    kps2, des2 = Img2.findFeatures('sift')   
+
+    # Extract the masks to filter the features into several clusters
+    masks1 = utils.getMaskPointsInROIs(kps1, ROIs1)
+    masks2 = utils.getMaskPointsInROIs(kps2, ROIs2)
+
+    kpsCluster1, desCluster1 = Img1.featureCluster(masks1)
+    kpsCluster2, desCluster2 = Img2.featureCluster(masks2)
+
+    # Match the features with corresponding clusters in each image
+    matches = utils.clusterMatch(desCluster1,desCluster2)
+
+    # Show the number of matches
+    matchNum = 0
+    for i in range(len(matches)):
+        matchNum += len(matches[i])
+        print("-- Number of original matches in each area", len(matches[i]))
+    print("Number of original total matches: ", matchNum)
+
+    # draw the matches in each cluster
+    utils.drawMatch(Img1,kpsCluster1,Img2,kpsCluster2,matches,draw_params)
+    
     plt.show()
