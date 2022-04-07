dfaf95e900fa2b2782cd6c28b4bc9cdda9883041
Kyle-Xu001 <xuchenghao10@hotmail.com>
2022-04-07 16:39:16 +0200

    simplify the real-time video stitching

--- stitch_custom.py
+++ stitch_custom.py
@@ -12,11 +12,12 @@ from utils import getHomoParams
 from pathlib import Path
 from vid_sync import VideoSynchronizer
 
+# Create visualize window for Videos
 def create_window(window_name):
-    
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)    # Create window with freedom of dimensions
     cv2.resizeWindow(window_name, 800,600)
-    
+
+# Define the function to Load the arguments
 def parse_args():
     ap = argparse.ArgumentParser(description='Run multi-camera stitching')
 
@@ -29,28 +30,32 @@ def parse_args():
     args = vars(ap.parse_args())
 
     return args 
+
+# Define the image stitching method for every frame
 def stitchImages(imgs, homo_params):
-    img_stitch_right = ImageStitch.simpleStitch(imgs["lamp15"], imgs["lamp14"], np.array(homo_params["lamp15-lamp14"]).reshape(-1, 3))
-    img_stitch_right = ImageStitch.simpleStitch(imgs["lamp16"], img_stitch_right, np.array(homo_params["lamp16-lamp15"]).reshape(-1, 3))
-    img_stitch_right = ImageStitch.simpleStitch(imgs["lamp17"], img_stitch_right, np.array(homo_params["lamp17-lamp16"]).reshape(-1, 3))
-    img_stitch_right = ImageStitch.simpleStitch(imgs["lamp18"], img_stitch_right, np.array(homo_params["lamp18-lamp17"]).reshape(-1, 3))
-    img_stitch_right = cv2.warpPerspective(img_stitch_right, np.array(homo_params["Right_Transform"]).reshape(-1, 3), (img_stitch_right.shape[1], img_stitch_right.shape[0]))
+    img_stitch_right = ImageStitch.simpleStitch(imgs["lamp15"], imgs["lamp14"], homo_params["lamp15-lamp14"])
+    img_stitch_right = ImageStitch.simpleStitch(imgs["lamp16"], img_stitch_right, homo_params["lamp16-lamp15"])
+    img_stitch_right = ImageStitch.simpleStitch(imgs["lamp17"], img_stitch_right, homo_params["lamp17-lamp16"])
+    img_stitch_right = ImageStitch.simpleStitch(imgs["lamp18"], img_stitch_right, homo_params["lamp18-lamp17"])
+    img_stitch_right = cv2.warpPerspective(img_stitch_right, homo_params["Right_Transform"], (img_stitch_right.shape[1], img_stitch_right.shape[0]))
     
-    img_stitch_left = ImageStitch.simpleStitch(cv2.flip(imgs["lamp22"], 0), cv2.flip(imgs["lamp23"], 0), np.array(homo_params["lamp23-lamp22"]).reshape(-1, 3))
-    img_stitch_left = ImageStitch.simpleStitch(cv2.flip(imgs["lamp21"], 0), img_stitch_left, np.array(homo_params["lamp22-lamp21"]).reshape(-1, 3))
-    img_stitch_left = ImageStitch.simpleStitch(cv2.flip(imgs["lamp20"], 0), img_stitch_left, np.array(homo_params["lamp21-lamp20"]).reshape(-1, 3))
-    img_stitch_left = ImageStitch.simpleStitch(cv2.flip(imgs["lamp19"], 0), img_stitch_left, np.array(homo_params["lamp20-lamp19"]).reshape(-1, 3))
+    img_stitch_left = ImageStitch.simpleStitch(cv2.flip(imgs["lamp22"], 0), cv2.flip(imgs["lamp23"], 0), homo_params["lamp23-lamp22"])
+    img_stitch_left = ImageStitch.simpleStitch(cv2.flip(imgs["lamp21"], 0), img_stitch_left, homo_params["lamp22-lamp21"])
+    img_stitch_left = ImageStitch.simpleStitch(cv2.flip(imgs["lamp20"], 0), img_stitch_left, homo_params["lamp21-lamp20"])
+    img_stitch_left = ImageStitch.simpleStitch(cv2.flip(imgs["lamp19"], 0), img_stitch_left, homo_params["lamp20-lamp19"])
     img_stitch_left = cv2.flip(img_stitch_left, 0)
-    img_stitch_left = cv2.warpPerspective(img_stitch_left, np.array(homo_params["Left_Transform"]).reshape(-1, 3), (img_stitch_left.shape[1], img_stitch_left.shape[0]))
+    img_stitch_left = cv2.warpPerspective(img_stitch_left, homo_params["Left_Transform"], (img_stitch_left.shape[1], img_stitch_left.shape[0]))
     
-    img_stitch = ImageStitch.simpleStitch(img_stitch_left, img_stitch_right, np.array(homo_params["stitch_total"]).reshape(-1, 3))
+    img_stitch = ImageStitch.simpleStitch(img_stitch_left, img_stitch_right, homo_params["stitch_total"])
     img_stitch = img_stitch[500:5300,50:1450,:]
     return img_stitch
 
 
 def stitch_all_frames(args):
+    # Define the path to the video group (from lamp14-lamp23)
     vid_path = args["input_vid_group_path"]
     
+    # Define the function about saving the stitching video
     save = False
     if args["save_path"] == True:
         save = True
@@ -61,10 +66,12 @@ def stitch_all_frames(args):
         
         save_video_path = os.path.join(args["save_path"],"stitched",vid_path.split('/')[-1]+"_stitch.mp4")
 
+    # Load the homography transform parameters
     homo_params = getHomoParams(args["stitch_params_path"])
-    stream_ids = sorted(list(homo_params.keys()))
-
+    for homo_param in homo_params:
+        homo_params[homo_param] = np.array(homo_params[homo_param]).reshape(-1, 3)
     
+    # Initialize the stitching to estimate the parameters
     vid_sync_test = VideoSynchronizer(vid_path, use_distorted=False, skip_init_frames=0)
     frameset = vid_sync_test.getFrames()
     imgs = frameset.imgs
@@ -76,22 +83,24 @@ def stitch_all_frames(args):
     height = img_stitch.shape[0]
     if save:
         writer = cv2.VideoWriter(save_video_path, fourcc, fps, (width, height))
-        
+    
+    # Create OpenCV windows for each videos    
     window_name = "stitched_image"
     create_window(window_name)
     
-    for img_src in imgs.keys():
-        create_window(img_src)
+    # for img_src in imgs.keys():
+    #     create_window(img_src)
 
+    # Start Real-Time Stitching
     vid_sync = VideoSynchronizer(vid_path,use_distorted=False,skip_init_frames=0)
     
     while(True):
         frameset = vid_sync.getFrames()
         imgs = frameset.imgs
-        for img_src, img_arr in imgs.items():
-            cv2.imshow(img_src,img_arr)
+        # for img_src, img_arr in imgs.items():
+        #     cv2.imshow(img_src,img_arr)
             
-        img_stitch = stitchImages(imgs,homo_params)
+        img_stitch = stitchImages(imgs, homo_params)
         img_stitch = cv2.rotate(img_stitch,cv2.ROTATE_90_COUNTERCLOCKWISE)
         
         cv2.imshow(window_name,img_stitch)
